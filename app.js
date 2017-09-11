28970e2a542ed9dd354393589b564a538b139f53
marcos.penha <git.12qwaszx>
2017-09-11 15:26:14 -0300

    Adicionando o relatório CSV

--- app.js
+++ app.js
@@ -121,6 +121,53 @@ app.service("PfService", function() {
     this.getPontosFuncao = function() {
         return _qtdPontosFuncao;
     }
+
+    this.converteParaCSV = function(requisitos) {
+        var array = typeof requisitos != 'object' ? JSON.parse(requisitos) : requisitos;
+        var str = '';
+        for (var i = 0; i < array.length; i++) {
+            var line = '';
+            for (var index in array[i]) {
+                if (line != '') line += ','
+    
+                line += array[i][index];
+            }
+    
+            str += line + '\r\n';
+        }
+    
+        return str;
+    }
+
+    this.exportaCsv = function(headers, items, fileTitle) {
+        if (headers) {
+            items.unshift(headers);
+        }
+    
+        // Convert Object to JSON
+        var jsonObject = JSON.stringify(items);
+    
+        var csv = this.converteParaCSV(jsonObject);
+    
+        var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+    
+        var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+        if (navigator.msSaveBlob) { // IE 10+
+            navigator.msSaveBlob(blob, exportedFilenmae);
+        } else {
+            var link = document.createElement("a");
+            if (link.download !== undefined) { // feature detection
+                // Browsers that support HTML5 download attribute
+                var url = URL.createObjectURL(blob);
+                link.setAttribute("href", url);
+                link.setAttribute("download", exportedFilenmae);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
+        }
+    }
 });
 
 app.controller("requisitosCtrl", ["$scope", "$mdToast", "$mdDialog", "ConfiguracoesService", "PfService", function($scope, $mdToast, $mdDialog, ConfiguracoesService, PfService) {
@@ -179,6 +226,10 @@ app.controller("requisitosCtrl", ["$scope", "$mdToast", "$mdDialog", "Configurac
             .position('top right')
             .hideDelay(5000)
         );
+    }
+
+    $scope.exportaCsv = function() {
+        PfService.exportaCsv(Object.keys($scope.requisitos[0]), $scope.requisitos, "relatorio");
     }
 
     $scope.formEditarRequisito = function(requisito) {
